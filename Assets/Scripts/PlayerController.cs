21d24eb2f2a74d6f2cbf6608fcc49dae85b8bf9d
Wildthing77 <55107076+Wildthing77@users.noreply.github.com>
2019-09-16 16:15:57 -0400

    additions
    removed rigidbody from character and placed a character controller onto character and changed the code on the script

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,18 +5,18 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed = 50.0f;// determines how fast the character will move around
+    private CharacterController characterController; // creates an instance variable to store characterController
     // Start is called before the first frame update
     void Start()
     {
-        
+        characterController = GetComponent<CharacterController>(); // refrences the component 
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 pos = transform.position;// gets the current position of the space marine
-        pos.x += moveSpeed * Input.GetAxis("Horizontal") * Time.deltaTime;// controls movement left and right then multiplied by moveSpeed to get current position and reads how long it took to get to the new position
-        pos.z += moveSpeed * Input.GetAxis("Vertical") * Time.deltaTime;// controls movement up and down then multiplies by moveSpeed to get current position and reads how long it took to get to the new position
-        transform.position = pos; // Updates space marine's location
+        Vector3 moveDirection = new Vector3(Input.GetAxis("Horizontal"), // creates a new vector3 to store movement direction then calls SimpleMove() and passes
+            0, Input.GetAxis("Vertical"));                               // in moveDirection * moveSpeed
+        characterController.SimpleMove(moveDirection * moveSpeed);
     }
 }
