2836baf2d64777cb5e2876303dc7aa53b599e4f3
Wildthing77 <55107076+Wildthing77@users.noreply.github.com>
2019-09-20 13:18:14 -0400

    made a projectile script on the projectile prefab

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    private void OnBecameInvisible() { Destroy(gameObject); // destroys the bullets when they are no longer visible.
+    }
+    private void OnCollisionEnter(Collision collision) // called whenever the bullets makes collision with another gameobject, then destroys the bullet.
+    {
+      Destroy(gameObject);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
