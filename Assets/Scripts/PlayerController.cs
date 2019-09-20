eadbceb5932c0f9c76cf74d7bdd09a26f9eb1c65
Wildthing77 <55107076+Wildthing77@users.noreply.github.com>
2019-09-20 14:07:58 -0400

    added a void fix function to finish getting the spacemarine to bob

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public Rigidbody head;
     public float moveSpeed = 50.0f;// determines how fast the character will move around
     private CharacterController characterController; // creates an instance variable to store characterController
     // Start is called before the first frame update
@@ -18,5 +19,16 @@ public class PlayerController : MonoBehaviour
         Vector3 moveDirection = new Vector3(Input.GetAxis("Horizontal"), // creates a new vector3 to store movement direction then calls SimpleMove() and passes
             0, Input.GetAxis("Vertical"));                               // in moveDirection * moveSpeed
         characterController.SimpleMove(moveDirection * moveSpeed);
+    }
+    void FixedUpdate() // handles physics.Used because were using force to move the bobblehead
+    {
+        Vector3 moveDirection = new Vector3(Input.GetAxis("Horizontal"), // moves the head when you move spacemarine.
+            0, Input.GetAxis("Vertical")); // calculates direction of movement
+        if (moveDirection == Vector3.zero) // see if value equals vector3.zero if so marine stays still.
+        {   // TODO 
+        }  else
+        {
+            head.AddForce(transform.right * 150, ForceMode.Acceleration); // gets the head to bobble by multipling direction by force.
+        }
     }
 }
