2772965ee23bd9d057f0310d9ab14fe437b2a009
Wildthing77 <55107076+Wildthing77@users.noreply.github.com>
2019-09-21 23:08:54 -0400

    Added a gameManger and added some code to get the aliens to spawn

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject player;  //hero's game object
+    public GameObject[] spawnPoints;// alien spawn point locations
+    public GameObject alien; // prefab for alien
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
