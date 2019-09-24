575846c2c755b27b2f2c3495162ce0b9466d44e3
Wildthing77 <55107076+Wildthing77@users.noreply.github.com>
2019-09-23 22:04:28 -0400

    made a ArenaWalls script and coded in the triggered walls where they go down when space marine goes down and up when he leaves triggered area

---
+++ Assets/Scripts/ArenaWall.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArenaWall : MonoBehaviour
+{
+    private Animator arenaAnimator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject arena = transform.parent.gameObject;// gets parent gameobject
+        arenaAnimator = arena.GetComponent<Animator>();// once it gets the reference gameobject it calls the getcomponent to reference the animator
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerEnter(Collider other)
+    {
+     arenaAnimator.SetBool("IsLowered", true);// when IsLowered is activated this sets it to true
+    }
+    void OnTriggerExit(Collider other)
+    {
+     arenaAnimator.SetBool("IsLowered", false);// raises the wall when the space marine leaves the trigger
+    }
+}
