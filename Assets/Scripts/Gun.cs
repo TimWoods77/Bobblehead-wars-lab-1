d1a614de2302f28cef45103e3a6bc1d4ab61d0b1
Wildthing77 <55107076+Wildthing77@users.noreply.github.com>
2019-09-25 21:04:02 -0400

    created bullet and upgraded gun

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -7,16 +7,43 @@ public class Gun : MonoBehaviour
     public GameObject bulletPrefab;
     public Transform launchPosition;
     private AudioSource audioSource;
+    public bool isUpgraded;// a flag that let's the script know to fire one or three bullets
+    public float upgradeTime = 10.0f;// amount of time the upgrade lasts
+    private float currentTime;//keeps track of how long the gun has been upgraded for
+
+    private Rigidbody createBullet()
+    {
+        GameObject bullet = Instantiate(bulletPrefab) as GameObject;// It encapsulates the bullet creation process. 
+        bullet.transform.position = launchPosition.position;
+        return bullet.GetComponent<Rigidbody>();//Returns a rigidbody after you create the bullet
+    }
 
     void fireBullet()
-    {   // 1   
-        GameObject bullet = Instantiate(bulletPrefab) as GameObject; // creates a gameObject instance for a particular prefab
-        // 2  
-        bullet.transform.position = launchPosition.position;
-        // 3   
-        bullet.GetComponent<Rigidbody>().velocity = transform.parent.forward * 100;
-        audioSource.PlayOneShot(SoundManager.Instance.gunFire);// plays shooting sound
+    {
+        Rigidbody bullet = createBullet(); bullet.velocity = transform.parent.forward * 100;// creates the bullet
+
+        if (isUpgraded)
+        {
+            Rigidbody bullet2 = createBullet(); bullet2.velocity = (transform.right + transform.forward / 0.5f) * 100;// fires the next two bullets at angles. It calculates the angle by adding the forward direction to either
+            Rigidbody bullet3 = createBullet(); bullet3.velocity = ((transform.right * -1) + transform.forward / 0.5f) * 100;//  the right- or left-hand direction and dividing in half. Since there is no explicit left property, you multiply the value of right by -1 to negate it.
+        }
+        if (isUpgraded)
+        {
+            audioSource.PlayOneShot(SoundManager.Instance.upgradedGunFire);// plays shooting sound
+        }
+        else
+        {
+            audioSource.PlayOneShot(SoundManager.Instance.gunFire);// plays shooting sound
+        }
     }
+
+    public void UpgradeGun()
+    {
+        isUpgraded = true;// lets the gun know it's been upgraded
+        currentTime = 0; // sets counter to 0
+    }
+
+
     // Start is called before the first frame update
     void Start()
     {
